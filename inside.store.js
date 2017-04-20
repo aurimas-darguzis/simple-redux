e34ccd5bbadc60ef2d8a8fe8637d461d302fc0ee
aurimas-darguzis <aurimas@darguzis.com>
2017-04-20 19:24:42 +0100

    redux store implementation

--- inside.store.js
+++ inside.store.js
@@ -1,16 +1,3 @@
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-
-    case 'DECREMENT':
-      return state - 1
-
-    default:
-      return state
-  }
-}
-
 const createStore = (reducer) => {
   let state
   let listeners = []
@@ -31,18 +18,3 @@ const createStore = (reducer) => {
 
   return { getState, dispatch, subscribe }
 }
-
-
-
-const store = createStore(counter)
-
-const render = () => {
-  document.body.innerText = store.getState()
-}
-
-store.subscribe(render)
-render()
-
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' })
-})
--- redux-principles.js
+++ redux-principles.js
@@ -8,9 +8,8 @@
 const SHOW_ROOT_ENTITY_TREE = 'SHOW_ROOT_ENTITY_TREE'
 const SHOW_CHILD_ENTITY_TREE = 'SHOW_CHILD_ENTITY_TREE'
 
-const OPEN_DETAILS_TAB_IS = 'OPEN_DETAILS_TAB_IS'
-const CLOSE_DETAILS_TAB_IS = 'CLOSE_DETAILS_TAB_IS'
-
+const OPEN_DETAILS_TAB = 'OPEN_DETAILS_TAB_IS'
+const CLOSE_DETAILS_TAB = 'CLOSE_DETAILS_TAB_IS'
 
 const SHOW_ENTITY = 'SHOW_ENTITY'
 const ADD_ENTITY = 'ADD_ENTITY'
@@ -28,12 +27,28 @@ const CLOSE_SIDE_BAR = 'CLOSE_SIDE_BAR'
 const OPEN_SEARCH_BAR = 'OPEN_SEARCH_BAR'
 const CLOSE_SEARCH_BAR = 'CLOSE_SEARCH_BAR'
 
-/* 
+export { SHOW_ROOT_ENTITY_TREE,
+         SHOW_CHILD_ENTITY_TREE,
+         OPEN_DETAILS_TAB,
+         CLOSE_DETAILS_TAB,
+         SHOW_ENTITY,
+         ADD_ENTITY,
+         EDIT_ENTITY,
+         DELETE_ENTITY,
+         SHOW_TAG,
+         ADD_TAG,
+         EDIT_TAG,
+         DELETE_TAG,
+         OPEN_SIDE_BAR,
+         CLOSE_SIDE_BAR,
+         OPEN_SEARCH_BAR,
+         CLOSE_SEARCH_BAR}
+/*
   The state is read only. The only way to change - dispatch an action.
-  An action is plain javascript obejct, describing in the minimum way what 
+  An action is plain javascript obejct, describing in the minimum way what
   changed in app eager by netwrok request or user interaction.
 
-  When you want to change the state - you need to dispatch 
+  When you want to change the state - you need to dispatch
   an action. Action will have a type. Since strings are easy to
   serialize - we will use them.
 
@@ -43,5 +58,5 @@ const CLOSE_SEARCH_BAR = 'CLOSE_SEARCH_BAR'
   To describe state mutations you have to write a function
   that takes a previous state of the app, the action being dispatched
   and returns the next state of the app. And this function has to be pure.
-  This function is called - the reducer. 
+  This function is called - the reducer.
  */
