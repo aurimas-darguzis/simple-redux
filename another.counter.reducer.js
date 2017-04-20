e3b39b3b7b98523b24449b3af07b109c251b1e27
aurimas-darguzis <aurimas@darguzis.com>
2017-04-20 19:39:38 +0100

    another counter reducer

---
+++ another.counter.reducer.js
@@ -0,0 +1,14 @@
+const addCounter = (list) => {
+
+}
+
+const testAddCounter = () => {
+  const listBefore = []
+  const listAfter = []
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter)
+}
+
+testAddConter()
+console.log('All tests passed!')
